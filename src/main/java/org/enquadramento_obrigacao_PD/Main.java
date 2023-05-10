7080e1ee6c6d7353135f126561ea3c78cb6c3b6d
Ênio Neves de Souza <61443786268@szfm.local>
2023-05-10 14:30:04 -0400

    Fix variables' identifiers

--- src/main/java/org/enquadramento_obrigacao_PD/Main.java
+++ src/main/java/org/enquadramento_obrigacao_PD/Main.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Double> Dispendio_ICTsPublicas = fetchData.exec("ICTsPublicas");
-        List<Double> Dispendio_ICTsPrivadas = fetchData.exec("ICTsPrivadas");
+        List<Double> aportes_ICTsPublicas = fetchData.exec("ICTsPublicas");
+        List<Double> aportes_ICTsPrivadas = fetchData.exec("ICTsPrivadas");
         List<Double> Obrigacao_PD = fetchData.exec("Obrigacao_P&D");
 
-        printData.exec(Dispendio_ICTsPublicas, "ICT Pública", "*** Dispêndios com ICTs Públicas ***");
-        printData.exec(Dispendio_ICTsPrivadas, "ICT Privada", "*** Dispêndios com ICTs Privadas ***");
+        printData.exec(aportes_ICTsPublicas, "ICT Pública", "*** Dispêndios com ICTs Públicas ***");
+        printData.exec(aportes_ICTsPrivadas, "ICT Privada", "*** Dispêndios com ICTs Privadas ***");
         printData.exec(Obrigacao_PD, "Obrigação", "*** Total de Obrigações de P&D ***");
 
-        VerificaObrigacoesAtual.exec(Dispendio_ICTsPublicas, Dispendio_ICTsPrivadas, Obrigacao_PD);
+        VerificaObrigacoesAtual.exec(aportes_ICTsPublicas, aportes_ICTsPrivadas, Obrigacao_PD);
         System.out.println("\n\n");
-        VerificaObrigacoesProposta.exec(Dispendio_ICTsPublicas, Dispendio_ICTsPrivadas, Obrigacao_PD);
+        VerificaObrigacoesProposta.exec(aportes_ICTsPublicas, aportes_ICTsPrivadas, Obrigacao_PD);
     }
 }
--- src/main/java/org/enquadramento_obrigacao_PD/VerificaObrigacoesProposta.java
+++ src/main/java/org/enquadramento_obrigacao_PD/VerificaObrigacoesProposta.java
@@ -4,46 +4,46 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class VerificaObrigacoesProposta {
-    static void exec(List<Double> Dispendio_ICTsPublicas, List<Double> Dispendio_ICTsPrivadas, List<Double> Obrigacao_PD) {
-        Double totalDispendiosICTsPublicas = sum(Dispendio_ICTsPublicas);
-        Double totalDispendiosICTsPrivadas = sum(Dispendio_ICTsPrivadas);
+    static void exec(List<Double> aportes_ICTsPublicas, List<Double> aportes_ICTsPrivadas, List<Double> Obrigacao_PD) {
+        Double totalAportesICTsPublicas = sum(aportes_ICTsPublicas);
+        Double totalAportesICTsPrivadas = sum(aportes_ICTsPrivadas);
         Double totalObrigacao_PD = sum(Obrigacao_PD);
 
         Double ObrigacaoIncI = totalObrigacao_PD * 0.9 / 5;
         Double ObrigacaoIncVI = totalObrigacao_PD * 0.4 / 5;
 
-        Double DispendiosIncI = totalDispendiosICTsPrivadas;
-        Double DispendiosIncVI = 0.0;
+        Double aportesIncI = totalAportesICTsPrivadas;
+        Double aportesIncVI = 0.0;
 
-        if (((totalDispendiosICTsPublicas + totalDispendiosICTsPrivadas) >= (ObrigacaoIncI + ObrigacaoIncVI))
-                && (totalDispendiosICTsPublicas >= ObrigacaoIncVI)) {
-            if (DispendiosIncI < ObrigacaoIncI) {
-                DispendiosIncI = ObrigacaoIncI;
-                DispendiosIncVI = totalDispendiosICTsPublicas - (ObrigacaoIncI - totalDispendiosICTsPrivadas);
+        if (((totalAportesICTsPublicas + totalAportesICTsPrivadas) >= (ObrigacaoIncI + ObrigacaoIncVI))
+                && (totalAportesICTsPublicas >= ObrigacaoIncVI)) {
+            if (aportesIncI < ObrigacaoIncI) {
+                aportesIncI = ObrigacaoIncI;
+                aportesIncVI = totalAportesICTsPublicas - (ObrigacaoIncI - totalAportesICTsPrivadas);
             } else {
-                DispendiosIncVI = totalDispendiosICTsPublicas;
+                aportesIncVI = totalAportesICTsPublicas;
             }
-        } else if (totalDispendiosICTsPublicas < ObrigacaoIncVI) {
-            DispendiosIncVI = totalDispendiosICTsPublicas;
+        } else if (totalAportesICTsPublicas < ObrigacaoIncVI) {
+            aportesIncVI = totalAportesICTsPublicas;
         } else {
-            DispendiosIncI += totalDispendiosICTsPublicas - ObrigacaoIncVI;
-            DispendiosIncVI = ObrigacaoIncVI;
+            aportesIncI += totalAportesICTsPublicas - ObrigacaoIncVI;
+            aportesIncVI = ObrigacaoIncVI;
         }
 
-        System.out.printf("TOTAL DE DISPÊNDIOS COM ICTs PÚBLICAS: %.2f\n", totalDispendiosICTsPublicas);
-        System.out.printf("TOTAL DE DISPÊNDIOS COM ICTs PRIVADAS: %.2f\n", totalDispendiosICTsPrivadas);
+        System.out.printf("TOTAL DE DISPÊNDIOS COM ICTs PÚBLICAS: %.2f\n", totalAportesICTsPublicas);
+        System.out.printf("TOTAL DE DISPÊNDIOS COM ICTs PRIVADAS: %.2f\n", totalAportesICTsPrivadas);
         System.out.printf("TOTAL DE OBRIGAÇÕES DE P&D: %.2f\n", totalObrigacao_PD);
         System.out.printf("OBRIGAÇÃO DE P&D, conf inc I do § 1º do art 5º do Decreto 10.521/2020: %.2f\n", ObrigacaoIncI);
-        System.out.printf("DISPÊNDOS DE P&D, conf inc I do § 1º do art 5º do Decreto 10.521/2020: %.2f\n", DispendiosIncI);
+        System.out.printf("DISPÊNDIOS DE P&D, conf inc I do § 1º do art 5º do Decreto 10.521/2020: %.2f\n", aportesIncI);
         System.out.printf("OBRIGAÇÃO DE P&D, conf inc VI do § 1º do art 5º do Decreto 10.521/2020: %.2f\n", ObrigacaoIncVI);
-        System.out.printf("DISPÊNDOS DE P&D, conf inc VI do § 1º do art 5º do Decreto 10.521/2020: %.2f\n", DispendiosIncVI);
+        System.out.printf("DISPÊNDIOS DE P&D, conf inc VI do § 1º do art 5º do Decreto 10.521/2020: %.2f\n", aportesIncVI);
 
-        if (DispendiosIncI >= ObrigacaoIncI) {
+        if (aportesIncI >= ObrigacaoIncI) {
             System.out.println("Empresa adimplente conforme Inciso I do Decreto 10.521/2020 - Forma Proposta!");
         }
         else System.out.println("Empresa inadimplente conforme Inciso I do Decreto 10.521/2020 - Forma Proposta!");
 
-        if (DispendiosIncVI >= ObrigacaoIncVI) {
+        if (aportesIncVI >= ObrigacaoIncVI) {
             System.out.println("Empresa adimplente conforme Inciso VI do Decreto 10.521/2020 - Forma Proposta!");
         }
         else System.out.println("Empresa inadimplente conforme Inciso VI do Decreto 10.521/2020 - Forma Proposta!");
@@ -52,7 +52,7 @@ public class VerificaObrigacoesProposta {
     }
 
     static Double sum(List<Double> list) {
-        AtomicReference<Double> total = new AtomicReference<>((double) 0);
+        AtomicReference<Double> total = new AtomicReference<>(0.0);
         list.forEach((Double num) -> {
             total.updateAndGet(v -> v + num);
         });
